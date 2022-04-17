7c5a634f32ffcd9e7edef5feff9e393b290bb80b
futherus <simankovich.al@phystech.edu>
2022-04-17 07:19:13 +0300

    Fixed warning

--- src/List.cpp
+++ src/List.cpp
@@ -48,7 +48,7 @@ static int list_resize_(List* list, int new_cap)
     
     if(NODS != nullptr)
     {
-        memcpy(tmp_ptr, NODS, (CAP + 1) * sizeof(Node));
+        memcpy(tmp_ptr, NODS, ((size_t) CAP + 1) * sizeof(Node));
         free(NODS);
     }
 
